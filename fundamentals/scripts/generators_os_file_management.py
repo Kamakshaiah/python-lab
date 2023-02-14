6085f6224db98795cd09193c0b883bf634976394
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2023-02-14 12:37:10 +0530

    updated os, file management and generators

--- fundamentals/scripts/generators_os_file_management.py
+++ fundamentals/scripts/generators_os_file_management.py
@@ -63,3 +63,25 @@ filegen = csv_reader('file.txt')
 for i in range(5):
     print(next(filegen))
 
+## application - palindrome
+
+def is_palindrome(num):
+    # Skip single-digit inputs
+    if num // 10 == 0:
+        return False
+    temp = num
+    reversed_num = 0
+
+    while temp != 0:
+        reversed_num = (reversed_num * 10) + (temp % 10)
+        temp = temp // 10
+
+    if num == reversed_num:
+        return num
+    else:
+        return False
+    
+for i in infinite_sequence():
+    pal = is_palindrome(i)
+    if pal:
+        print(i)
