d2c8bf954be325202b572bf9d170908edd49d13f
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2023-02-28 09:56:57 +0530

    updated x^y in lambdademos script

---
+++ Functions-demos/lambdademos.py
@@ -0,0 +1,12 @@
+from operator import pow
+from itertools import repeat
+list(map(pow, [1,2,3], repeat(4)))
+
+list(map(pow, [1,2,3], repeat(2)))
+
+import operator
+operator.__lt__(1, 2)
+
+operator.__lt__(2, 1)
+
+operator.__add__(1, 2)
