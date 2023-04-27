21be7715b99a62663551591346c2b9288d0a50b2
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2023-04-27 15:46:27 +0530

    updated covid-19 script

--- scipy-pandas/covid_19_crosstabs.py
+++ scipy-pandas/covid_19_crosstabs.py
@@ -10,5 +10,9 @@ covid_19_treatment.groupby('effect').count()
 covid_19_treatment.groupby('effect').mean()
 covid_19_treatment.groupby('effect').get_group('yes')
 covid_19_treatment.groupby('effect').get_group('no')
+
 pd.crosstab(covid_19_treatment.remdesvir, covid_19_treatment.azithromycin)
 sp.stats.chi2_contingency(pd.crosstab(covid_19_treatment.remdesvir, covid_19_treatment.azithromycin)) # Chi2ContingencyResult(statistic=4.155917159763314, pvalue=0.38531580762893597, dof=4, expected_freq=array([[5.2       , 4.        , 2.8       ],        [2.16666667, 1.66666667, 1.16666667],        [5.63333333, 4.33333333, 3.03333333]])) 
+
+ct = pd.crosstab([df['remdesvir'], df['azythromycin']], df['effect'])
+sp.stats.chi2_contingency(ct)
